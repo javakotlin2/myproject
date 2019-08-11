084039ff28e3944596d0e280d325379beec030da
Hank Tom <hanktom@gmail.com>
2019-08-11 15:47:29 +0800

    Initial commit

---
+++ src/main/kotlin/com/hank/Hello.kt
@@ -0,0 +1,14 @@
+package com.hank
+
+fun main() {
+//    println("Hello Kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
